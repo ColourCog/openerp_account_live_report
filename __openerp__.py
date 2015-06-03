5bde1ff32a9c50ba4642f2b1b6e85a7b914af094
Stanislas H.B. Sodonon <bidossessi@linuxbenin.com>
2015-06-03 23:53:28 +0100

    initial report

---
+++ __openerp__.py
@@ -0,0 +1,31 @@
+{
+    "name" : "Accounting Live Reports",
+    "version" : "0.1",
+    "category" : "Accounting",
+    "sequence": 60,
+    "complexity" : "normal",
+    "author" : "ColourCog.com",
+    "website" : "http://colourcog.com",
+    "depends" : [
+        "base",
+        "account_accountant",
+    ],
+    "summary" : "Generate realtime accounting reports",
+    "description" : """
+Accounting Live Reports
+=======================
+This module creates computed accounting snapshots.
+
+Features:
+---------
+* Profit/Loss
+* Balance Sheet
+    """,
+    "data" : [
+      "security/ir.model.access.csv",
+      'report_view.xml',
+    ],
+    "application": False,
+    "installable": True
+}
+
